aac71f083d7f9e9d0f26526019dc4e63be034743
Edanpelo <47367370+Edanpelo@users.noreply.github.com>
2021-01-26 16:00:37 -0500

    Update urls.py

--- api_compraventa/urls.py
+++ api_compraventa/urls.py
@@ -10,6 +10,4 @@ urlpatterns = [
     path('Articulos/<int:pk>', views.detalles_articulo),        # Operaciones basicas CRUD para el articulo
     path('En_venta/', views.en_venta_list),                     # Ver articulos en venta y comprar articulos
     path('Prendas/', views.prenda_list),                        # Ver todas las prendas y crear prendas
-    # path('En_venta/comprar/', views.comprar_item),
-    # path('Crear_prenda/<int:nit>/<int:pk>', views.crear_prenda),
 ]
